e1352dcae66808f299452f92065c2a1029c47bcd
gianluigi7 <gianluigiferraioli7@gmail.com>
2024-05-21 20:02:27 +0200

    milestone 2

--- main.js
+++ main.js
@@ -24,17 +24,57 @@ const images = [
     }
 ];
 
-   let carosello = document.querySelector('.items');
-   for (let i = 0; i < images.length; i++) {
-       
-       
-       let divItem = document.createElement('div');
-       divItem.className = 'item active';
-    let immagine = document.createElement('img');
-    immagine.src = images[i].image;
-    divItem.append(immagine);
-    carosello.append(divItem);
+let currentPosition = 0;
+const items = document.querySelector('.items');
+createElements();
 
-  }
+const allItems = document.querySelectorAll('.item');
 
-  console.log(posizione.image);
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+
+
+function createElements() {
+    images.forEach((element, index, array) => {
+        console.log(element);
+      
+        const div = document.createElement('div');
+        div.className = 'item';
+      
+        if(index === currentPosition) {
+          div.classList.add('active');
+        }
+      
+        const image = document.createElement('img');
+        image.src = element.image;
+        image.alt = element.title;
+      
+        div.append(image);
+        items.append(div);
+      });
+}
+
+  
+   prevButton.addEventListener('click', function() {
+
+       allItems[currentPosition].classList.remove('active');
+     if (currentPosition > 0) {
+         currentPosition--;
+    } else { 
+        currentPosition = allItems.lenght - 1
+    }
+        allItems[currentPosition].classList.add('active');
+
+   });
+   nextButton.addEventListener('click', function() {
+    
+       allItems[currentPosition].classList.remove('active');
+    if (currentPosition < allItems.lenght - 1) {
+        currentPosition++;
+    } else {
+        currentPosition = 0;
+    }
+    allItems[currentPosition].classList.add('active');
+
+     
+   });
